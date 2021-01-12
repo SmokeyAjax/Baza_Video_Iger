3000a2177d5e53165fa3bf4ab2c1041b76ea10ae
SmokeyAjax <44201359+SmokeyAjax@users.noreply.github.com>
2021-01-12 15:55:22 +0100

    Add files via upload

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -17,6 +17,22 @@ def igra(igra):
         podatki_o_igri=Igre.podatki_o_igri(igra)
     )
 
+@bottle.get('/isci/')
+def iskanje():
+    iskalni_niz = bottle.request.query.getunicode('iskalni_niz')
+    igre = Igre.poisci(iskalni_niz)
+    return bottle.template(
+        'html/iskanje.html',
+        iskalni_niz = iskalni_niz,
+        igre = igre
+    )
+
+@bottle.get('/glej_vse_igre/')
+def glej_vse_igre():
+    return bottle.template(
+        'html/glej_vse_igre.html',
+        glej_vse_igre=Igre.glej_vse_igre()
+    )
 
 
 bottle.run(debug=True, reloader=True)
